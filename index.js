5eb0ff173a90a4e49e0ce43f9a33803605560339
Rahul Routh <bluewebsurfer@gmail.com>
2021-03-23 11:10:06 +0530

    commercial 1

---
+++ index.js
@@ -0,0 +1,25 @@
+var rellax = new Rellax('.rellax');
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+// top nav
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+ 
